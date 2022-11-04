734b738669c614b5360599a5f01a7c04bd17a6de
Hydroxyapatite <beepingbooper@gmail.com>
2022-11-03 22:47:04 -0700

    Add compatibility with implants mod

--- Patches/Biotech/PatchMechanitorTracker_CanCommandTo.cs
+++ Patches/Biotech/PatchMechanitorTracker_CanCommandTo.cs
@@ -1,35 +1,38 @@
 ﻿using HarmonyLib;
 using RimWorld;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Verse;
 
-namespace MechPsyControlRange_Hydroxyapatite.Patches.Biotech
+namespace MechPsyControlRange_Hydroxyapatite
 {
     [HarmonyPatch(
-        typeof(Pawn_MechanitorTracker), 
-        nameof(Pawn_MechanitorTracker.CanCommandTo), 
-        new Type[] { typeof(LocalTargetInfo) })]
+            typeof(Pawn_MechanitorTracker),
+            nameof(Pawn_MechanitorTracker.CanCommandTo),
+            new Type[] { typeof(LocalTargetInfo) })]
     internal class PatchMechanitorTracker_CanCommandTo
     {
+        static Pawn_MechanitorTracker mechanitor;
+        static int? lastCacheTick = null;
+        static float range = 0;
         static void Postfix(
             Pawn_MechanitorTracker __instance,
             LocalTargetInfo target,
             ref bool __result
             )
         {
-            if (!target.Cell.InBounds(__instance.Pawn.MapHeld))
+            mechanitor = __instance;
+            if (!target.Cell.InBounds(mechanitor.Pawn.MapHeld))
             {
                 __result = false;
             }
-            float defaultRange = 620.01f;
-            float psySensSq = (float) Math.Pow(__instance.Pawn.GetStatValue(StatDefOf.PsychicSensitivity, applyPostProcess: true, 1), 2);
-            float maxRange = (float) Math.Pow(Math.Floor(GenRadial.MaxRadialPatternRadius), 2);
-            float range = Math.Min(defaultRange * psySensSq, maxRange);
-            __result = __instance.Pawn.Position.DistanceToSquared(target.Cell) < range;
+            int currentTick = Find.TickManager.TicksGame;
+            if (!lastCacheTick.HasValue || currentTick - lastCacheTick > 60)
+            {
+                lastCacheTick = currentTick;
+                range = (float)Math.Pow(mechanitor.Pawn.GetStatValue(MechanitorDefOf.Hydroxyapatite_MechCommandDistance, applyPostProcess: true, 60), 2);
+            }
+            float maxRange = (float)Math.Pow(Math.Floor(GenRadial.MaxRadialPatternRadius), 2);
+            __result = mechanitor.Pawn.Position.DistanceToSquared(target.Cell) < Math.Min(range, maxRange);
         }
     }
 }
--- Patches/Biotech/PatchMechanitorTracker_DrawCommandRadius.cs
+++ Patches/Biotech/PatchMechanitorTracker_DrawCommandRadius.cs
@@ -1,30 +1,37 @@
 ﻿using HarmonyLib;
 using RimWorld;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using UnityEngine;
 using Verse;
 
-namespace MechPsyControlRange_Hydroxyapatite.Patches.Biotech
+namespace MechPsyControlRange_Hydroxyapatite
 {
     [HarmonyPatch(
-        typeof(Pawn_MechanitorTracker), 
-        nameof(Pawn_MechanitorTracker.DrawCommandRadius))]
+            typeof(Pawn_MechanitorTracker),
+            nameof(Pawn_MechanitorTracker.DrawCommandRadius))]
     internal class PatchMechanitorTracker_DrawCommandRadius
     {
+        static Pawn_MechanitorTracker mechanitor;
+        delegate bool commandDel(LocalTargetInfo info);
+        static commandDel commandToDel;
+        static int? lastCacheTick = null;
+        static float range = 0;
         static bool Prefix(
             Pawn_MechanitorTracker __instance
             )
         {
             if (__instance.Pawn.Spawned && __instance.AnySelectedDraftedMechs)
             {
-                float psySens = __instance.Pawn.GetStatValue(StatDefOf.PsychicSensitivity, applyPostProcess: true, 1);
-                float defaultRange = 24.9f;
-                float range = Math.Min(psySens * defaultRange, (float) Math.Floor(GenRadial.MaxRadialPatternRadius));
-                GenDraw.DrawRadiusRing(__instance.Pawn.Position, range, Color.white, (IntVec3 c) => __instance.CanCommandTo(c));
+                int currentTick = Find.TickManager.TicksGame;
+                mechanitor = __instance;
+                commandToDel = __instance.CanCommandTo;
+                if (!lastCacheTick.HasValue || currentTick - lastCacheTick > 60)
+                {
+                    lastCacheTick = currentTick;
+                    range = mechanitor.Pawn.GetStatValue(MechanitorDefOf.Hydroxyapatite_MechCommandDistance, applyPostProcess: true, 60);
+                }
+                range = Math.Min(range, (float)Math.Floor(GenRadial.MaxRadialPatternRadius));
+                GenDraw.DrawRadiusRing(mechanitor.Pawn.Position, range, Color.white, (IntVec3 c) => commandToDel(c));
             }
             return false;
         }
